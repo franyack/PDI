58353bf8465e4704490e6986d920e57406594e47
Fran <fyackel@gmail.com>
2017-03-21 16:57:18 -0300

    Esperemos que este si ande de una vez!

---
+++ Codigos/Prueba1/MiProyecto/main.cpp
@@ -0,0 +1,36 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+
+using namespace cv;
+
+int main(int argc, char** argv) {
+	//create a gui window:
+	namedWindow("Output",1);
+//	//initialize a 120X350 matrix of black pixels:
+//	Mat output = Mat::zeros( 120, 350, CV_8UC3 );
+//	//write text on the matrix:
+//	putText(output,
+//		"Hello World :)",
+//		cvPoint(15,70),
+//		FONT_HERSHEY_PLAIN,
+//		3,
+//		cvScalar(0,255,0),
+//		4);
+//	//display the image:
+//	imshow("Output", output);
+//	//wait for the user to press any key:
+//	waitKey(0);
+	Mat img = imread("prueba2.png", CV_LOAD_IMAGE_GRAYSCALE);
+	
+	Mat output = img;
+	//Este es solo un comentario para ver la diferencia en Git
+	imshow("Output",output);
+	waitKey(0);
+	
+	//Este es otro comentario para seguir probando Git, pero ahora usando GitHub
+	
+	
+	
+	return 0;
+} 
---
+++ Codigos/Prueba1/MiProyecto/readme.txt
@@ -0,0 +1,14 @@
+Este proyecto está configurado para utilizar la biblioteca OpenCV (en su versión 
+2.4.10 en las configuraciones para Windows, en cualquier versión que se encuentre
+instalada en las configuraciones para GNU/Linux).
+
+El código de ejemplo del main fue tomado de https://gist.github.com/EyalAr/3940636
+
+Al final de la barra de herramientas encontrará una ícono adicional para acceder
+a la referencia en línea de la biblioteca.
+
+
+Ahora voy a hacer un cambio en el readme pero solo es para el curso de Git
+
+
+Este cambio seguro se va a ver en el branch test
