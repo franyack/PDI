cfd4406eb24172b1af90eb03622321f210df89d4
Fran <fyackel@gmail.com>
2017-04-05 10:52:34 -0300

    Se movio intro_OpenCV al directorio principal y se avanzo con la guia 2

--- Tema 2 - Operaciones en el dominio espacial/MiProyecto/main.cpp
+++ Tema 2 - Operaciones en el dominio espacial/MiProyecto/main.cpp
@@ -109,7 +109,41 @@ void TP2_Ejercicio2(int inciso,float gamma=1){
 	imshow("Output", output);
 	
 
-	waitKey(0);
+	
+}
+
+void TP2_Ejercicio3(){
+	///SUMA DE DOS IMAGENES
+	///LAS IMAGENES DEBEN SER DEL MISMO TAMAÑO, POR LO TANTO HAY QUE DEFINIR UN ROI DE IGUAL DE MAGNITUD DE LAS MISMAS. ADEMAS, SE LAS PUEDE PONDERAR
+	///COMO SE MUESTRA EN EL EJEMPLO DE ABAJO
+	
+	
+	Mat img1 = imread("huang1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Mat img2 = imread("huang2.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Rect region_of_interest = Rect(1, 1, 256, 256);
+	img1 = img1(region_of_interest);
+	Mat result = (img1 + img2)/2;
+	imshow("Suma", result);
+	
+	///RESTA Y REESCALADOS TIPICOS PARA EVITAR DESBORDES DE RANGO
+	
+	result = (img1 - img2 + 255)/2;
+	imshow("Resta", result);
+	
+	///MULTIPLICACION
+	
+	//Mat mask = Mat::zeros(img1.size(),img1.type());
+	
+	//circle(mask,cv::Point(mask.rows/2,mask.cols/2),50,Scalar(255,0,0),-1,8,0);
+	
+//	Rect mask = Rect(1,1,256,256);
+//	Mat roi = img1(mask);
+//	
+//	
+//	imshow("Mascara", roi);
+//	
+	
+	
 }
 
 
@@ -117,6 +151,10 @@ int main(int argc, char** argv) {
 	
 	//TP2_Ejercicio1(100,100);
 	
-	TP2_Ejercicio2(2,0.5);
+	//TP2_Ejercicio2(2,0.5);
+	
+	TP2_Ejercicio3();
+	
+	waitKey(0);
 	return 0;
 } 
