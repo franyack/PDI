a1c9f2de2e55cc077d406ad2265225cf32ea2aff
Fran <fyackel@gmail.com>
2017-04-06 17:05:41 -0300

    Falta la division del ejercicio 3.1 y falta continuar el 3.2

--- Tema 2 - Operaciones en el dominio espacial/MiProyecto/main.cpp
+++ Tema 2 - Operaciones en el dominio espacial/MiProyecto/main.cpp
@@ -3,9 +3,26 @@
 #include <vector>
 #include "pdi_functions.h"
 #include "utils.h"
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
 
 using namespace cv;
 using namespace pdi;
+using namespace std;
+
+float generarRandom(float a, float b){
+	std::mt19937_64 rng;
+	// initialize the random number generator with time-dependent seed
+	uint64_t timeSeed = rand();//std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	std::seed_seq ss{uint32_t(timeSeed & 0xffffffff), uint32_t(timeSeed>>32)};
+	rng.seed(ss);
+	// initialize a uniform distribution between 0 and 1
+	std::uniform_real_distribution<double> unif(a, b);
+	double currentRandomNumber = unif(rng);
+	return currentRandomNumber;
+}
 
 
 void TP2_Ejercicio1(float a,float c){
@@ -70,12 +87,11 @@ void TP2_Ejercicio2(int inciso,float gamma=1){
 	imshow("Foto original",img);
 	
 	Mat lut(1,256,CV_8U);
-	
+	float c = 255.0/log(256);
 	float aux;
 	if (inciso == 1){
 		for(int i=0;i<255;i++){
-			
-			aux = log(1+i);///255.0)*255;
+			aux = c*log(1+i);
 			if(aux>255) aux = 255;
 			if(aux<0) aux = 0;
 			lut.at<unsigned char>(i)= aux;
@@ -83,7 +99,7 @@ void TP2_Ejercicio2(int inciso,float gamma=1){
 	}else{
 		for(int i=0;i<255;i++){
 			
-			aux = pow(i,gamma);
+			aux = pow(i/255.0,gamma)*255.0;
 			if(aux>255) aux = 255;
 			if(aux<0) aux = 0;
 			lut.at<unsigned char>(i)= aux;
@@ -103,14 +119,17 @@ void TP2_Ejercicio2(int inciso,float gamma=1){
 	
 	LUT(img,lut,output);
 	
-	normalize(output,output,0,255,CV_MINMAX);
+	//normalize(output,output,0,255,CV_MINMAX);
 	
 	//display the image:
 	imshow("Output", output);
 	
+	
+	
 
 	
 }
+
 
 void TP2_Ejercicio3(){
 	///SUMA DE DOS IMAGENES
@@ -132,6 +151,15 @@ void TP2_Ejercicio3(){
 	
 	///MULTIPLICACION
 	
+	Mat mask= Mat::zeros(img1.size(),img1.type());
+	
+	mask(Rect(img1.rows/2-50,img1.cols/2-50,100,100)) = 1;
+	
+	Mat resultado;
+	
+	img1.copyTo(resultado,mask);
+	
+	imshow("Resultado",resultado);
 	//Mat mask = Mat::zeros(img1.size(),img1.type());
 	
 	//circle(mask,cv::Point(mask.rows/2,mask.cols/2),50,Scalar(255,0,0),-1,8,0);
@@ -147,13 +175,39 @@ void TP2_Ejercicio3(){
 }
 
 
+void TP2_Ejercicio3_2(){
+	Mat img = imread("huang1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	imshow("Imagen Original",img);
+	Mat aux(300,300,CV_8U);
+	
+	float aux2;
+	for (int i=0;i<aux.rows;i++){
+		for(int j=0;j<aux.cols;j++){
+			aux2 = aux.at<uchar>(i,j)+generarRandom(0,0.05);
+			if(aux2>255) aux2 = 255;
+			img.at<uchar>(i,j)=aux2;
+			cout<<(float)aux.at<uchar>(i,j)<<endl;
+		}
+	}
+	//imshow("Imagen contaminada",img);
+	
+}
+
+
+
 int main(int argc, char** argv) {
 	
 	//TP2_Ejercicio1(100,100);
 	
-	//TP2_Ejercicio2(2,0.5);
+//	TP2_Ejercicio2(2,2);
 	
 	TP2_Ejercicio3();
+	
+//	TP2_Ejercicio3_2();
+	
+	
+
+	
 	
 	waitKey(0);
 	return 0;
