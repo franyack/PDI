deb1459611dd6965380648db0d4bfeb8832b8e8b
Fran <fyackel@gmail.com>
2017-04-18 10:37:34 -0300

    Realizacion del ejercicio 4 y del ejercicio 5.2

--- Tema 2 - Operaciones en el dominio espacial/MiProyecto/main.cpp
+++ Tema 2 - Operaciones en el dominio espacial/MiProyecto/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
+#include <bitset>
 
 using namespace cv;
 using namespace pdi;
@@ -176,21 +177,164 @@ void TP2_Ejercicio3(){
 
 
 void TP2_Ejercicio3_2(){
-	Mat img = imread("huang1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
-	imshow("Imagen Original",img);
-	Mat aux(300,300,CV_8U);
+//	Mat img = imread("huang1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+//	imshow("Imagen Original",img);
+//	vector<Mat>imagenes;
+//	
+//	for(int i=0;i<50;i++){
+//		Mat ruido = Mat::zeros(img.rows,img.cols,CV_8U);
+//		randn(ruido,0,0.05);
+//		Mat resultado = (img+ruido);
+//		imagenes.push_back(resultado);
+//	}
+//	Mat promedio = imagenes[0];
+//
+//	for(int i=1;i<imagenes.size();i++){
+//		promedio+=imagenes[i];
+//	}
+//	
+//	promedio/=imagenes.size();
+//	
+//	imshow("Promedio",promedio);
+//	
+//	
+////	float aux2;
+////	for (int i=0;i<aux.rows;i++){
+////		for(int j=0;j<aux.cols;j++){
+////			aux2 = aux.at<uchar>(i,j)+generarRandom(0,0.05);
+////			if(aux2>255) aux2 = 255;
+////			img.at<uchar>(i,j)=aux2;
+////			cout<<(float)aux.at<uchar>(i,j)<<endl;
+////		}
+////	}
+//	//imshow("Imagen contaminada",img);
 	
-	float aux2;
-	for (int i=0;i<aux.rows;i++){
-		for(int j=0;j<aux.cols;j++){
-			aux2 = aux.at<uchar>(i,j)+generarRandom(0,0.05);
-			if(aux2>255) aux2 = 255;
-			img.at<uchar>(i,j)=aux2;
-			cout<<(float)aux.at<uchar>(i,j)<<endl;
+	Mat img=imread("huang1.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	//	Mat ruido(img.size(),img.type());
+	//	randn(ruido,0,0.5*255);
+	Mat aux;
+	img.convertTo(aux,CV_32F);
+	vector<Mat> imagenes;
+	for (int i=0;i<50;i++){
+		Mat ruido(img.size(),img.type());
+		randn(ruido,0,0.5*255);
+		Mat resultado=(aux+ruido)/2;
+		imagenes.push_back(resultado);
+	}
+	Mat mezcla=imagenes[0];
+	for (int i=1;i<imagenes.size();i++){
+		mezcla+=imagenes[i];
+	}
+	mezcla/=imagenes.size();
+	imshow("Original",img);
+	imshow("Ruido",imagenes[0]);
+	imshow("Resultado",mezcla);
+	
+}
+
+void TP2_Ejercicio4(){
+	
+	Mat input_img(imread("huang1.jpg", CV_LOAD_IMAGE_GRAYSCALE));
+	imshow("Original",input_img);
+	//Clono la imagen..
+	Mat work_img(input_img.clone());
+	//Creo vector de Mat
+	vector<Mat>BitPlane;
+	//Creo un string con el nombre de archivo
+	string file_name("huang2_bit0.png");
+	for (int i=0; i < 8; i++) {
+		Mat outPut;
+		//Aplico mascara AND para acceder a cada bit plane (2^i)
+		bitwise_and(work_img, pow(2,i), outPut);
+		//Multiplico por 255 para hacerlo blanco y negro
+		outPut *= 255;
+		BitPlane.push_back(outPut);
+		//Escribo la imagen en el disco
+		//		cv::imwrite(file_name, outPut);
+		//		//Modifico el nombre del archivo
+		//		file_name[10] += 1;
+	}
+	//Muestro todas las imagenes en una sola pantalla
+	//mosaic(Vector,cantidadFilas);
+	Mat Graficar=mosaic(BitPlane,2);
+	imshow("Bit Planes 0 - 7",Graficar);
+	///2da Parte Construya una imagen solo con la informacion del plano del bit mas
+	///significativo, luego construya otra imagen con la informacion de los dos planos 
+	///mas significativos, y asi sucesivamente
+	vector<Mat>SumaBitPlanes;
+	int cont=0;
+	for(int i=7;i>=0;i--) {
+		Mat Aux(input_img.rows,input_img.cols,CV_8UC(1),Scalar(0));
+		if(i<7){
+			//Sumatoria de BitPlanes anteriores con actual
+			//A cada bitplane lo tengo que multiplicar con pow(2,i) para pasar de binario
+			//a un numero entero
+			//La primera vez que entra cont=0 
+			//Entonces Suma BitPlane[6] con SumaBitPlanes[0] (que tiene el valor del Bit 7)
+			//La segunda vez que entra cont=1
+			//Entonces Suma BitPlane[5] con SumaBitPlanes[1] (que tiene la suma de Bit 6 y 7)
+			//Asi sucesivamente..
+			Aux=(BitPlane[i]/255)*pow(2,i)+(SumaBitPlanes[cont]);
+			SumaBitPlanes.push_back(Aux);
+			cont++;
+		}else{
+			//La primera vez entra aca Bit7
+			SumaBitPlanes.push_back((BitPlane[i]/255)*pow(2,i));
 		}
 	}
-	//imshow("Imagen contaminada",img);
 	
+	Mat Graficar2=mosaic(SumaBitPlanes,2);
+	imshow("Sumatoria BitPlane",Graficar2);
+}
+
+
+void TP2_Ejercicio5_2(Mat entrada){
+	Mat img1=imread("a7v600-SE.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	Mat img2=imread("a7v600-X.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	
+	
+	imshow("SE",img1);
+	imshow("X",img2);
+	
+	Mat mask= Mat::zeros(img2.size(),img2.type());
+	mask(Rect(img2.rows/2-10,img2.cols/2-120,100,100)) = 1;
+	Mat resultadox;
+	img2.copyTo(resultadox,mask);
+	
+	Mat maskx= Mat::zeros(img1.size(),img1.type());
+	maskx(Rect(img1.rows/2-10,img1.cols/2-120,100,100)) = 1;
+	Mat resultadose;
+	img1.copyTo(resultadose,maskx);
+	
+	imshow("ss",resultadose);
+	float x,se = 0;
+	
+	for(int i=150;i<250;++i){
+		x += (float)resultadox.at<uchar>(116,i);
+		se += (float)resultadose.at<uchar>(116,i);
+	}
+	x /= (250-150);
+	se /= (250-150);
+	
+	cout<<"Promedio de X: "<<x<<endl<<"Promedio de SE: "<<se<<endl;
+	
+	Mat mask_input= Mat::zeros(entrada.size(),entrada.type());
+	mask_input(Rect(entrada.rows/2-10,entrada.cols/2-120,100,100)) = 1;
+	Mat resultado_entrada;
+	entrada.copyTo(resultado_entrada,mask_input);
+	
+	float in = 0;
+	
+	for(int i=150;i<250;++i){
+		in += (float)resultado_entrada.at<uchar>(116,i);
+	}
+	in /= (250-150);
+	cout<<"Promedio de la entrada: "<<in<<endl<<endl;
+	if(abs(in - x) < abs(in - se)){
+		cout<<"La placa es A7V600-x"<<endl;
+	}else{
+		cout<<"La placa es A7V600-SE"<<endl;
+	}
 }
 
 
@@ -201,13 +345,14 @@ int main(int argc, char** argv) {
 	
 //	TP2_Ejercicio2(2,2);
 	
-	TP2_Ejercicio3();
+//	TP2_Ejercicio3();
 	
 //	TP2_Ejercicio3_2();
 	
+//	TP2_Ejercicio4();
 	
-
-	
+	Mat entrada = imread("a7v600-SE-ruido.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+	TP2_Ejercicio5_2(entrada);
 	
 	waitKey(0);
 	return 0;
